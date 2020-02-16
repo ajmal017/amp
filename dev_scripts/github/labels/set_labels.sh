a07615f86a1c7b2897e12aa3103acb3c14357eca
saggese <saggese@gmail.com>
2020-02-16 15:25:07 -0500

    Improve

--- dev_scripts/github/labels/set_labels.sh
+++ dev_scripts/github/labels/set_labels.sh
@@ -6,21 +6,35 @@ SRC_DIR="./dev_scripts/github/labels"
 DST_DIR="$SRC_DIR/backup"
 #gh_tech_labels.json
 
+
+function label() {
+    FULL_OPTS="$OPTS -o $OWNER -r $REPO"
+    CMD="python $EXEC $FULL_OPTS -t $GH_TOKEN"
+    echo "> $CMD"
+    eval $CMD
+    echo "Done"
+}
+
+
 # Backup.
+if [[ 0 == 1 ]]; then
 OWNER="ParticleDev"
 REPO="commodity_research"
 FILE_NAME="$DST_DIR/labels.$OWNER.$REPO.json"
-OPTS="-d $FILE_NAME -o $OWNER -r $REPO"
-CMD="python $EXEC $OPTS -t $GH_TOKEN"
-echo "> $CMD"
-eval $CMD
-echo "Done"
+OPTS="-d $FILE_NAME"
+label
+
+FILE_NAME="$SRC_DIR/gh_tech_labels.json"
+OPTS="-m $FILE_NAME"
+label
+fi;
 
 OWNER="alphamatic"
 REPO="amp"
 FILE_NAME="$DST_DIR/labels.$OWNER.$REPO.json"
-OPTS="-d $FILE_NAME -o $OWNER -r $REPO"
-CMD="python $EXEC $OPTS -t $GH_TOKEN"
-echo "> $CMD"
-eval $CMD
-echo "Done"
+OPTS="-d $FILE_NAME"
+label
+
+FILE_NAME="$SRC_DIR/gh_tech_labels.json"
+OPTS="-m $FILE_NAME"
+label
