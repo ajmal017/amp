2e3f998b4359cc582954dd1246f4f9c13cd58308
saggese <saggese@gmail.com>
2020-02-16 12:24:11 -0500

    Improve

--- dev_scripts/github/labels/set_labels.sh
+++ dev_scripts/github/labels/set_labels.sh
@@ -1,9 +1,9 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 EXEC="/Users/saggese/src/github/github-label-maker/github-label-maker.py"
 
-SRC_DIR="dev_scripts/github/labels"
-DST_DIR="$LABEL_DIR/backup"
+SRC_DIR="./dev_scripts/github/labels"
+DST_DIR="$SRC_DIR/backup"
 #gh_tech_labels.json
 
 # Backup.
@@ -11,12 +11,16 @@ OWNER="ParticleDev"
 REPO="commodity_research"
 FILE_NAME="$DST_DIR/labels.$OWNER.$REPO.json"
 OPTS="-d $FILE_NAME -o $OWNER -r $REPO"
-CMD="python $EXEC $OPTS  -t $GH_TOKEN"
+CMD="python $EXEC $OPTS -t $GH_TOKEN"
 echo "> $CMD"
+eval $CMD
+echo "Done"
 
 OWNER="alphamatic"
 REPO="amp"
 FILE_NAME="$DST_DIR/labels.$OWNER.$REPO.json"
 OPTS="-d $FILE_NAME -o $OWNER -r $REPO"
-CMD="python $EXEC $OPTS  -t $GH_TOKEN"
+CMD="python $EXEC $OPTS -t $GH_TOKEN"
 echo "> $CMD"
+eval $CMD
+echo "Done"
