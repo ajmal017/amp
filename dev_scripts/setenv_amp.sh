ddd73f003cafce45c6e755000b89000caf472435
saggese <saggese@gmail.com>
2021-05-13 11:21:38 -0400

    Improve build system after issue with docker-compose

--- dev_scripts/setenv_amp.sh
+++ dev_scripts/setenv_amp.sh
@@ -11,8 +11,11 @@ AMP=$PWD
 # Virtual env
 # #############################################################################
 
-# This needs to be in sync with dev_scripts/client_setup/build.sh
-VENV_DIR="$HOME/src/venv/amp.client_venv"
+echo "# Activate virtual env"
+VENV_DIR="$HOME/src/venv/client_setup"
+cmd="source $VENV_DIR/bin/activate"
+echo "> $cmd"
+eval $cmd
 
 echo "# Activate virtual env '$VENV_DIR'"
 source $VENV_DIR/bin/activate
