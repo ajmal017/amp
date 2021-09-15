80d7f8a2cd5a663bd73515bf9e2e78a7a62d74a1
paul <smith.paul.anthony@gmail.com>
2021-09-15 17:57:25 -0400

    Add tests

--- core/dataflow/nodes/test/test_transformers.py
+++ core/dataflow/nodes/test/test_transformers.py
@@ -65,6 +65,52 @@ class TestSeriesToSeriesTransformer(hut.TestCase):
         return data
 
 
+class TestSeriesToDfTransformer(hut.TestCase):
+    """
+
+    """
+    def test1(self) -> None:
+        def add_lags(srs: pd.Series, num_lags: int) -> pd.DataFrame:
+            lags = []
+            for lag in range(0, num_lags):
+                lags.append(srs.shift(lag).rename("lag_" + str(lag)))
+            out_df = pd.concat(lags, axis=1)
+            return out_df
+
+        data = self._get_data()
+        config = cconfig.get_config_from_nested_dict(
+            {
+                "in_col_group": ("close",),
+                "out_col_group": (),
+                "transformer_func": add_lags,
+                "transformer_kwargs": {"num_lags": 3, },
+            }
+        )
+        node = cdnt.SeriesToDfTransformer("add_lags", **config.to_dict())
+        actual = node.fit(data)["df_out"]
+        expected_txt = """
+,lag_0,lag_0,lag_1,lag_1,lag_2,lag_2,close,close,volume,volume
+,MN0,MN1,MN0,MN1,MN0,MN1,MN0,MN1,MN0,MN1
+2016-01-04 09:30:00,94.7,100.2,,,,,94.7,100.2,30000,40000
+2016-01-04 09:31:00,94.9,100.25,94.7,100.2,,,94.9,100.25,35000,44000
+2016-01-04 09:32:00,95.35,100.23,94.9,100.25,94.7,100.2,95.35,100.23,40000,45000
+"""
+        expected = pd.read_csv(io.StringIO(expected_txt), index_col=0, parse_dates=True,
+                               header=[0, 1])
+        hut.compare_df(actual, expected)
+
+    def _get_data(self) -> pd.DataFrame:
+        txt = """
+,close,close,volume,volume
+datetime,MN0,MN1,MN0,MN1
+2016-01-04 09:30:00,94.70,100.20,30000,40000
+2016-01-04 09:31:00,94.90,100.25,35000,44000
+2016-01-04 09:32:00,95.35,100.23,40000,45000
+"""
+        df = pd.read_csv(io.StringIO(txt), index_col=0, parse_dates=True, header=[0, 1])
+        return df
+
+
 class TestFunctionWrapper(hut.TestCase):
     def test1(self) -> None:
         """
