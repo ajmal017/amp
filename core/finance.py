8c99638f7cd3ca2531ddfe51b48b7de865365b24
Paul <smith.paul.anthony@gmail.com>
2021-11-09 10:24:58 -0500

    Amp task1825 pivot result df (#1829)
    * Checkpoint
    
    * Lint
    
    * Import updates
    
    * Update comments
    
    * Lint

--- core/finance.py
+++ core/finance.py
@@ -1204,3 +1204,86 @@ def maybe_resample(srs: pd.Series) -> pd.Series:
         _LOG.debug("No `freq` detected; resampling to 'B'.")
         srs = srs.resample("B").sum(min_count=1)
     return srs
+
+
+def stack_prediction_df(
+    df: pd.DataFrame,
+    id_col: str,
+    close_price_col: str,
+    vwap_col: str,
+    ret_col: str,
+    prediction_col: str,
+    ath_start: datetime.time,
+    ath_end: datetime.time,
+    remove_weekends: bool = True,
+) -> pd.DataFrame:
+    """
+    Process and stack a dataframe of predictions and financial data.
+
+    :param df: dataframe of the following form:
+        - DatetimeIndex with `freq`
+            - datetimes are end-of-bar datetimes (e.g., so all column values
+              are knowable at the timestamp, modulo the computation time
+              required for the prediction)
+        - Two column levels
+            - innermost level consists of the names/ids
+            - outermost level has features/market data
+    :param id_col: name to use for identifier col in output
+    :param close_price_col: col name for bar close price
+    :param vwap_col: col name for vwap
+    :param ret_col: col name for percentage returns
+    :param prediction_col: col name for returns prediction
+    :param ath_start: active trading hours start time
+    :param ath_end: active trading hours end time
+    :param remove_weekends: remove any weekend data iff `True`
+    :return: dataframe of the following form:
+        - RangeIndex
+        - Single column level, with columns for timestamps, ids, market data,
+          predictions, etc.
+        - Predictions are moved from the end-of-bar semantic to
+          beginning-of-bar semantic
+        - Epoch and timestamps are for beginning-of-bar rather than end
+    """
+    # Avoid modifying the input dataframe.
+    df = df.copy()
+    # TODO(Paul): Make this more robust.
+    idx_name = df.columns.names[1]
+    # Reindex according to start time.
+    bar_start_ts = compute_bar_start_timestamps(df).rename("start_bar_et_ts")
+    df.index = bar_start_ts
+    bar_start_ts.index = bar_start_ts
+    epoch = compute_epoch(df).squeeze().rename("minute_index")
+    # Extract market data (price, return, vwap).
+    dfs = []
+    dfs.append(
+        df[[close_price_col]].rename(columns={close_price_col: "eob_close"})
+    )
+    dfs.append(df[[vwap_col]].rename(columns={vwap_col: "eob_vwap"}))
+    dfs.append(df[[ret_col]].rename(columns={ret_col: "eob_ret"}))
+    # Perform time shifts for previous-bar price and move prediction to
+    # begining-of-bar semantic.
+    dfs.append(
+        df[[close_price_col]]
+        .shift(1)
+        .rename(columns={close_price_col: "eopb_close"})
+    )
+    dfs.append(
+        df[[prediction_col]].shift(1).rename(columns={prediction_col: "alpha"})
+    )
+    # Consolidate the dataframes.
+    out_df = pd.concat(dfs, axis=1)
+    # Perform ATH time filtering.
+    out_df = out_df.between_time(ath_start, ath_end)
+    # Maybe remove weekends.
+    if remove_weekends:
+        out_df = out_df[out_df.index.day_of_week < 5]
+    # TODO: Handle NaNs.
+    # Stack data and annotate id column.
+    out_df = (
+        out_df.stack().reset_index(level=1).rename(columns={idx_name: id_col})
+    )
+    # Add epoch and bar start timestamps.
+    out_df = out_df.join(epoch.to_frame())
+    out_df = out_df.join(bar_start_ts.apply(lambda x: x.isoformat()).to_frame())
+    # Reset the index.
+    return out_df.reset_index(drop=True)
